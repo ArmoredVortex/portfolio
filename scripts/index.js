133e0cb7b6d105a88af007d360c2fac655e9cb50
ArmoredVortex <rachitpandey.gghs@gmail.com>
2021-02-04 10:51:55 +0530

    Added Cookie storage for themes

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+    let r = document.querySelector(':root');
+    let theme = getCookie('theme');
+    switch(theme){
+        case 'monospace':
+            r.style.setProperty('--base-text', '#9578D3');
+            r.style.setProperty('--bg-color', '#181C18');
+            r.style.setProperty('--hover-text', '#00CE7C');
+            break;
+        case 'light':
+            r.style.setProperty('--base-text', '#000000');
+            r.style.setProperty('--bg-color', '#c2c5cc');
+            r.style.setProperty('--hover-text', '#2D539E'); 
+            break;   
+        case 'wasp':
+            r.style.setProperty('--base-text', '#F7F700');
+            r.style.setProperty('--bg-color', '#000000');
+            r.style.setProperty('--hover-text', '#F3DD57');   
+            break; 
+        case 'redVelvet':
+            r.style.setProperty('--base-text', '#EC4C56');
+            r.style.setProperty('--bg-color', '#242933');
+            r.style.setProperty('--hover-text', '#F6F0E9');  
+            break;  
+    }
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
---
+++ scripts/themes.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    let r = document.querySelector(':root');
+    // --base-text :
+    // --hover-text :
+    // --bg-color :
+    let monospace = document.getElementById('monospace')
+    monospace.addEventListener('click',()=>{
+        r.style.setProperty('--base-text', '#9578D3');
+        r.style.setProperty('--bg-color', '#181C18');
+        r.style.setProperty('--hover-text', '#00CE7C');
+        setCookie('theme','monospace',365);
+    })
+    let redVelvet = document.getElementById('red-velvet')
+    redVelvet.addEventListener('click',()=>{
+        r.style.setProperty('--base-text', '#EC4C56');
+        r.style.setProperty('--bg-color', '#242933');
+        r.style.setProperty('--hover-text', '#F6F0E9');
+        setCookie('theme','redVelvet',365);
+    })
+    let light = document.getElementById('light')
+    light.addEventListener('click',()=>{
+        r.style.setProperty('--base-text', '#000000');
+        r.style.setProperty('--bg-color', '#c2c5cc');
+        r.style.setProperty('--hover-text', '#2D539E');
+        setCookie('theme','light',365);
+    })
+    let wasp = document.getElementById('wasp')
+    wasp.addEventListener('click',()=>{
+        r.style.setProperty('--base-text', '#F7F700');
+        r.style.setProperty('--bg-color', '#000000');
+        r.style.setProperty('--hover-text', '#F3DD57');
+        setCookie('theme','wasp',365);
+    })
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+}
